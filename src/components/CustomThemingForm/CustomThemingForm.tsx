1a6c85181b9b3b1440d65b1f9c63b560e8b09f9c
Ethan Standel <ethan@standel.io>
2022-09-19 23:07:20 -0400

    fix: make the way theming works more obvious

--- src/components/CustomThemingForm/CustomThemingForm.tsx
+++ src/components/CustomThemingForm/CustomThemingForm.tsx
@@ -37,40 +37,45 @@ export const CustomThemingForm = () => {
           )} />
         <span>Start using a custom theme</span>
       </label>
-      <Button class={styles.clear!} onClick={() => {
-        setPrimary(color.hexToRgb(basePalette.primary));
-        setSecondary(color.hexToRgb(basePalette.secondary));
-        setBgMain(color.hexToRgb(darkPalette.bgMain));
-        setbgCard(color.hexToRgb(darkPalette.bgCard));
-        setTxtMain(color.hexToRgb(darkPalette.txtMain));
-        setTxtHeavy(color.hexToRgb(darkPalette.txtHeavy));
-      }}>Clear custom theme back to base dark</Button>
-      <section class={styles.cards!}>
-        <Card padding>
-          <h2>Primary color</h2>
-          <ColorPicker color={primary()} setColor={setPrimary} />
-        </Card>
-        <Card padding>
-          <h2>Secondary color</h2>
-          <ColorPicker color={secondary()} setColor={setSecondary} />
-        </Card>
-        <Card padding>
-          <h2>Main background color</h2>
-          <ColorPicker color={bgMain()} setColor={setBgMain} />
-        </Card>
-        <Card padding>
-          <h2>Card background color</h2>
-          <ColorPicker color={bgCard()} setColor={setbgCard} />
-        </Card>
-        <Card padding>
-          <h2>Main text color</h2>
-          <ColorPicker color={txtMain()} setColor={setTxtMain} />
-        </Card>
-        <Card padding>
-          <h2>Heavy text color</h2>
-          <ColorPicker color={txtHeavy()} setColor={setTxtHeavy} />
-        </Card>
-      </section>
+
+      {themeMode() === "custom" && (
+        <>
+        <section class={styles.cards!}>
+          <Card padding>
+            <h2>Primary color</h2>
+            <ColorPicker color={primary()} setColor={setPrimary} />
+          </Card>
+          <Card padding>
+            <h2>Secondary color</h2>
+            <ColorPicker color={secondary()} setColor={setSecondary} />
+          </Card>
+          <Card padding>
+            <h2>Main background color</h2>
+            <ColorPicker color={bgMain()} setColor={setBgMain} />
+          </Card>
+          <Card padding>
+            <h2>Card background color</h2>
+            <ColorPicker color={bgCard()} setColor={setbgCard} />
+          </Card>
+          <Card padding>
+            <h2>Main text color</h2>
+            <ColorPicker color={txtMain()} setColor={setTxtMain} />
+          </Card>
+          <Card padding>
+            <h2>Heavy text color</h2>
+            <ColorPicker color={txtHeavy()} setColor={setTxtHeavy} />
+          </Card>
+        </section>
+        <Button class={styles.clear!} onClick={() => {
+          setPrimary(color.hexToRgb(basePalette.primary));
+          setSecondary(color.hexToRgb(basePalette.secondary));
+          setBgMain(color.hexToRgb(darkPalette.bgMain));
+          setbgCard(color.hexToRgb(darkPalette.bgCard));
+          setTxtMain(color.hexToRgb(darkPalette.txtMain));
+          setTxtHeavy(color.hexToRgb(darkPalette.txtHeavy));
+        }}>Clear custom theme back to base dark</Button>
+      </>
+    )}
     </>
   );
 }
